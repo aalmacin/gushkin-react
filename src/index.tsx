7551ec8a1f4601a9e3f07c7eff265d2221c660b6
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2020-08-02 13:35:12 -0400

    Setup auth0

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Auth0Provider domain="dev-e180oleh.auth0.com" clientId="1fxGsbucwZKcc9Y2A4PrP29i507f5Rni" redirectUri={window.location.origin}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Auth0Provider>
+  , document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
