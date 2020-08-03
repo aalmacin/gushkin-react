b9c736342572261036dc2de55d66fe5d994380b0
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2020-08-03 03:41:48 -0400

    Setup react to test if apollo client works with golang graphql

--- src/authentication/Profile/Profile.tsx
+++ src/authentication/Profile/Profile.tsx
@@ -1,12 +1,53 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import config from "../../auth_config.json";
+import { ApolloClient, createHttpLink, gql, InMemoryCache } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context';
 
 const Profile: React.FC = () => {
-  const { user, isAuthenticated } = useAuth0();
+  const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
+
+  const getToken = async () => {
+    const accessToken = await getAccessTokenSilently({
+      audience: config.audience
+    })
+
+    const client = new ApolloClient({
+      uri: "http://localhost:8080/query",
+      cache: new InMemoryCache(),
+      headers: {
+        authorization: `Bearer ${accessToken}`
+      }
+    })
+    client.query({
+      query: gql`
+        query GetActivities {
+          activities {
+            id
+            description
+            positive
+            fundAmt
+            actions {
+              id
+              actionTimestamp
+              activity {
+                id
+                description
+                positive
+                fundAmt
+              }
+            }
+          }
+        }
+      `
+    }).then(res => console.log(res))
+  }
 
   if (!isAuthenticated) {
     return null
   }
+
+  getToken()
 
   return (
     isAuthenticated && (
