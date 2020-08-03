5a0c60d11c47e1df2a6b57b5ca41130a62873cc8
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2020-08-03 16:31:07 -0400

    Added home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import { useAuth0 } from '@auth0/auth0-react';
+import React from 'react';
+import classes from './Home.module.scss';
 
-const Home = () => {
-  return <div>Home</div>
+function Home() {
+  const { isAuthenticated } = useAuth0()
+
+  return (
+    <div className={classes.Home}>
+      <h2>Home</h2>
+      <p>{isAuthenticated ? 'Logged in' : 'Not logged in'}</p>
+    </div>
+  )
 }
 
-export default Home
+export default Home;
