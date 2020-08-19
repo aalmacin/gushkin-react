9bde755eb3b56e7d8d92fb4ae3479405ab3c14a5
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2020-08-18 21:21:34 -0400

    Added active link

--- src/SideNav/SideNav.tsx
+++ src/SideNav/SideNav.tsx
@@ -1,13 +1,21 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import classes from './SideNav.module.scss';
 
 function SideNav() {
+  const { pathname } = useLocation()
+  const isActive = (link: string): boolean => {
+    if (pathname !== '/' && pathname.endsWith('/')) {
+      const check = pathname.substr(0, pathname.length - 1)
+      return check === link
+    }
+    return pathname === link
+  }
   return <div className={classes.SideNav}>
     <ul className={classes.SideNavItems}>
-      <li className={classes.SideNavItem}><Link to="/">Home</Link></li>
-      <li className={classes.SideNavItem}><Link to="/main/activities">Activities</Link></li>
-      <li className={classes.SideNavItem}><Link to="/main/store">Store</Link></li>
+      <li className={`${classes.SideNavItem} ${isActive('/') && classes.Active}`}><Link to="/">Home</Link></li>
+      <li className={`${classes.SideNavItem} ${isActive('/main/activities') && classes.Active}`}><Link to="/main/activities">Activities</Link></li>
+      <li className={`${classes.SideNavItem} ${isActive('/main/store') && classes.Active}`}><Link to="/main/store">Store</Link></li>
     </ul>
   </div>
 }
