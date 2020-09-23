466d7a1c04216806d2afa228edbddb22af82658e
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2020-09-23 08:54:29 -0400

    Using JSS to create button styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import classes from "./Button.module.scss";
+import { createUseStyles } from "react-jss";
+import { colors } from "components/variables";
 
 export enum ButtonType {
   primary = "Primary",
@@ -17,6 +18,64 @@ interface ButtonProps {
   type: ButtonType;
 }
 
+const getBackgroundColor = (type: string, hover = false) => {
+  switch (type) {
+    case ButtonType.primary:
+      return hover ? colors.primary.shade4 : colors.primary.shade3;
+  }
+};
+
+const getTextColor = (type: string, hover = false) => {
+  switch (type) {
+    case ButtonType.primary:
+      return hover ? colors.primary.tint9 : colors.white;
+  }
+};
+
+const getBorderColor = (type: string, hover = false) => {
+  switch (type) {
+    case ButtonType.primary:
+      return colors.primary.tint9;
+  }
+};
+
+const useStyle = createUseStyles({
+  Button: {
+    fontSize: "2rem",
+    padding: "1rem 2rem",
+    flex: "1",
+    flexShrink: "1",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    cursor: "pointer",
+    borderRadius: ({ isSquare }) => isSquare ? "0" : "2rem",
+
+    backgroundColor: ({ type }) => getBackgroundColor(type),
+    color: ({ type }) => getTextColor(type),
+    border: ({ type }) => `1px solid ${getBorderColor(type)}`,
+
+    "&:hover": {
+      backgroundColor: ({ type }) => getBackgroundColor(type, true),
+      color: ({ type }) => getTextColor(type, true),
+      border: ({ type }) => `1px solid ${getBorderColor(type, true)}`,
+    },
+  },
+  Icon: {
+    backgroundColor: "transparent",
+    color: colors.black,
+    border: "none",
+
+    "&:hover": {
+      color: colors.error.shade9,
+    }
+  },
+  Children: {},
+  IconChildrenMargin: {
+    marginLeft: "1rem",
+  }
+});
+
 const Button: React.FC<ButtonProps & React.HTMLProps<HTMLButtonElement>> = ({
   icon,
   type,
@@ -24,11 +83,11 @@ const Button: React.FC<ButtonProps & React.HTMLProps<HTMLButtonElement>> = ({
   children,
   ...props
 }) => {
+  const classes = useStyle({ isSquare, type });
   return (
     <button
       {...props}
-      className={`${classes.Button} ${classes[type]} ${isSquare &&
-        classes.Square}`}
+      className={classes.Button}
     >
       <span className={classes.Icon}>
         {icon && <FontAwesomeIcon icon={icon} />}
