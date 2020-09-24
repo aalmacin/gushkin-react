ba7b8be65aba08d8a8bb864f51c868892d963965
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2020-09-24 09:51:34 -0400

    Moved ExpiredMessage to JSS

--- src/components/ExpiredMessage/ExpiredMessage.tsx
+++ src/components/ExpiredMessage/ExpiredMessage.tsx
@@ -1,11 +1,30 @@
+import { colors } from "components/variables";
 import React from "react";
-import classes from "./ExpiredMessage.module.scss";
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles({
+  ExpiredMessage: {
+    position: "fixed",
+    top: "1 %",
+    left: "50 %",
+    transform: "translateX(-50 %)",
+    backgroundColor: colors.gold.tint4,
+    padding: "1rem 2rem",
+    borderRadius: "1rem",
+    color: colors.black.base,
+  },
+  Message: {
+    marginRight: "1rem",
+  }
+});
 
 interface ExpiredMessageProps {
-  isExpired?: boolean
+  isExpired?: boolean;
 }
 
 const ExpiredMessage: React.FC<ExpiredMessageProps> = ({ isExpired }) => {
+  const classes = useStyles();
+
   if (!isExpired) {
     return null;
   }
@@ -15,6 +34,6 @@ const ExpiredMessage: React.FC<ExpiredMessageProps> = ({ isExpired }) => {
       <a href='/'>Click here to refresh</a>
     </div>
   );
-}
+};
 
 export default ExpiredMessage;
