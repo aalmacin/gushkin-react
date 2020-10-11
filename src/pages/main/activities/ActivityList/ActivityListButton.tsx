44a49cf063aabbc838fe423001edcb98e9debb1c
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2020-10-11 14:13:41 -0400

    Fixed colors for buttons

--- src/pages/main/activities/ActivityList/ActivityListButton.tsx
+++ src/pages/main/activities/ActivityList/ActivityListButton.tsx
@@ -2,6 +2,7 @@ import React, { ButtonHTMLAttributes } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
 import { createUseStyles } from 'react-jss';
+import { colors } from 'components/variables';
 
 const useStyles = createUseStyles({
   PerformActivityButton: {
@@ -11,17 +12,13 @@ const useStyles = createUseStyles({
     fontSize: 24,
     padding: "0px 16px",
     lineHeight: '61px',
-    color: "#eff0ff",
+    color: colors.white.base,
+
+    background: (props) => props.positive ? colors.primary.shade5 : colors.red.shade3,
 
     "&:hover": {
       cursor: "pointer",
     }
-  },
-  PerformActivityButtonRed: {
-    background: "#876666",
-  },
-  PerformActivityButtonGreen: {
-    background: "#718766",
   },
 });
 
@@ -30,14 +27,13 @@ type ActivityListButtonProps = {
 };
 
 const ActivityListButton: React.FC<ActivityListButtonProps & ButtonHTMLAttributes<HTMLButtonElement>> = ({ positive, ...props }) => {
-  const classes = useStyles();
+  const classes = useStyles({
+    positive
+  });
   return (
     <button
       {...props}
-      className={`${classes.PerformActivityButton} ${positive
-        ? classes.PerformActivityButtonGreen
-        : classes.PerformActivityButtonRed
-        }`}
+      className={classes.PerformActivityButton}
     >
       <FontAwesomeIcon icon={positive ? faPlus : faMinus} />
     </button>
