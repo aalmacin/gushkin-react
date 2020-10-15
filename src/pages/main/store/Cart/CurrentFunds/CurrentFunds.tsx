2b20691ec9f0ceadf6dc812ac25de411cf6b3642
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2020-10-15 17:38:55 -0400

    refactor: current funds using jss

--- src/pages/main/store/Cart/CurrentFunds/CurrentFunds.tsx
+++ src/pages/main/store/Cart/CurrentFunds/CurrentFunds.tsx
@@ -1,13 +1,22 @@
 import React from "react";
-import classes from "./CurrentFunds.module.scss";
 
 import {
   faCoins
 } from "@fortawesome/free-solid-svg-icons";
 import Funds from "pages/main/components/funds";
 import HeaderIcon from "pages/main/components/headerIcon";
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles({
+  CurrentFunds: {
+    '& > *': {
+      margin: ['2rem', 0]
+    }
+  }
+});
 
 const CurrentFunds = () => {
+  const classes = useStyles();
   return (
     <div className={classes.CurrentFunds}>
       <HeaderIcon text="Your Funds" icon={faCoins} />
